ca288c95d0f4f520cb35eeb1f5dfca4fcbdec8ce
junio.albino <junioalbino@outlook.com>
2017-01-28 13:46:45 -0200

    Configuração inicial do projeto

---
+++ program.js
@@ -0,0 +1,10 @@
+var estiloTitulo = '.....................';
+function exibir(a) {
+    console.log(a);
+}
+exibir(estiloTitulo);
+exibir('Simulador de colméia');
+exibir(estiloTitulo);
+exibir('');
+exibir('');
+exibir('Seja um zangão, um soldado ou uma colheitadeira, viva o mundo da colméia!');
---
+++ program.ts
@@ -0,0 +1,12 @@
+let estiloTitulo = '.....................';
+
+function exibir(a) {
+    console.log(a);
+}
+
+exibir(estiloTitulo);
+exibir('Simulador de colméia');
+exibir(estiloTitulo);
+exibir('');
+exibir('');
+exibir('Seja um zangão, um soldado ou uma colheitadeira, viva o mundo da colméia!');

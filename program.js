84a8498a7cfa916936cba5af8020b228759af1c1
junio.albino <junioalbino@outlook.com>
2017-02-05 12:01:55 -0200

    Criado o projeto do npm

--- program.js
+++ program.js
@@ -1,4 +1,15 @@
+"use strict";
 var estiloTitulo = '.....................';
+var numAbelhas = 0;
+var rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Insira o número inicial de abelhas: ', function (answer) {
+    numAbelhas = answer;
+    console.log('Número de abelhas iniciais: ' + numAbelhas);
+    rl.close();
+});
 function exibir(a) {
     console.log(a);
 }
--- program.ts
+++ program.ts
@@ -1,4 +1,18 @@
-let estiloTitulo = '.....................';
+var estiloTitulo = '.....................';
+var numAbelhas = 0;
+
+import * as fs from "fs";
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Insira o número inicial de abelhas: ', (answer) => {
+    numAbelhas = answer;
+    console.log('Número de abelhas iniciais: ' + numAbelhas);
+    rl.close();
+});
 
 function exibir(a) {
     console.log(a);
